f96f56b92e77d7e6eb840a544e053be6cbbdb60b
Praneeth Paladugu <ppaladug@codeaurora.org>
2014-12-03 22:38:39 -0800

    msm: vidc: Fix a race condition in error handling
    Currently video driver handles session init and clean in one
    thread and response handling from HW in another thread. Due to
    race conditions response handler thread may access the session
    which was already freed by forward thread. Hence synchronize the
    forward thread and response handler with session_lock.
    
    Ex : When driver detects SYS_ERROR, it informs client and client
    tries to clean the session. In the meantime if HW sends a response,
    the response handler thread tries to access the inst struct and this
    struct might have freed as part of session close. This causes
    use-after-free exception.
    
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1454,7 +1454,6 @@ int msm_vidc_close(void *instance)
 	}
 
 	core = inst->core;
-	msm_comm_session_clean(inst);
 
 	mutex_lock(&core->lock);
 	list_for_each_safe(ptr, next, &core->instances) {
@@ -1481,6 +1480,8 @@ int msm_vidc_close(void *instance)
 	if (rc)
 		dprintk(VIDC_ERR,
 			"Failed to move video instance to uninit state\n");
+
+	msm_comm_session_clean(inst);
 
 	pr_info(VIDC_DBG_TAG "Closed video instance: %p\n",
 			VIDC_MSG_PRIO2STRING(VIDC_INFO), inst);
