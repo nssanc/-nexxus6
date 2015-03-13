72be07d3326ba0f54d91fa1ed425d9a9aa1a49b8
placiano <nextbluerom@outlook.es>
2015-03-13 01:14:24 +0100

    Revert "Kill Intellimm"
    This reverts commit aef6c5894b976a8b9a8aba0f0d82a74c6ecb2828.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -473,6 +473,9 @@ extern struct cpufreq_governor cpufreq_gov_hyperx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
+extern struct cpufreq_governor cpufreq_gov_intellimm;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellimm)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE_PRO)
 extern struct cpufreq_governor cpufreq_gov_interactive_pro;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_interactive_pro)
