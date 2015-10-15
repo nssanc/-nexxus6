41be443ed038ef979ecc15f8935346e7be6748cb
Joe Schubert <joseph.schubert@gmail.com>
2015-10-15 19:31:42 -0400

    Build Script changes Automate zip building.

--- build.sh
+++ build.sh
@@ -2,5 +2,21 @@
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=/media/joe/linux_storage/toolchains/arm-eabi-4.8/bin/arm-eabi-
+export LOCALVERSION="PlaceHolder_Name-Alpha"
+
+
 make shamu_defconfig
 make -j6
+
+if [ ! -f arch/arm/boot/zImage-dtb ]; then
+echo "Build Failed"
+else
+echo "Copy kernel and build generate anykernel zip."
+cp -vr arch/arm/boot/zImage-dtb zip/anykernel/kernel/zImage
+
+cd zip/anykernel
+zip -9 -r test_kernel.zip .
+cd ../..
+
+echo "Finished Zipping."
+fi
