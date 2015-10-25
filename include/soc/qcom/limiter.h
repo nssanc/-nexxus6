cff0b935d7bacadd1d5ad31800d77400388125f7
Pranav Vashi <neobuddy89@gmail.com>
2015-10-25 12:25:39 +0530

    soc: qcom: msm_limiter: Refactor limiter driver
    * Cleanup FB code. Only state notifier supported.
    * Cleanup suspend-resume workqueues - not required since
      state notifier driver does that for us.
    * Add debug prints.
    * Refactor and simplify updating cpu frequencies.
    * Rename functions to match with driver name.
    * Bump to 4.0
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	include/soc/qcom/limiter.h

--- include/soc/qcom/limiter.h
+++ include/soc/qcom/limiter.h
@@ -1,15 +1,8 @@
-#define MSM_LIMIT			"msm_limiter"
+#define MSM_LIMITER			"msm_limiter"
 #define LIMITER_ENABLED			1
-#define DEFAULT_SUSPEND_DEFER_TIME	10 
-#if defined(CONFIG_ARCH_APQ8084)
 #define DEFAULT_SUSPEND_FREQUENCY	960000
-#endif
-#if defined(CONFIG_ARCH_APQ8084)
 #define DEFAULT_RESUME_FREQUENCY	2726400
-#endif
-#if defined(CONFIG_ARCH_APQ8084)
 #define DEFAULT_MIN_FREQUENCY		300000
-#endif
 
 static struct cpu_limit {
 	unsigned int limiter_enabled;
@@ -18,11 +11,6 @@ static struct cpu_limit {
 	uint32_t resume_max_freq[4];
 	uint32_t suspend_min_freq_all;
 	uint32_t suspend_min_freq[4];
-	unsigned int suspended;
-	unsigned int suspend_defer_time;
-	struct delayed_work suspend_work;
-	struct work_struct resume_work;
-	struct mutex resume_suspend_mutex;
 	struct mutex msm_limiter_mutex[4];
 	struct notifier_block notif;
 } limit = {
@@ -38,5 +26,4 @@ static struct cpu_limit {
 	.suspend_min_freq[1] = DEFAULT_MIN_FREQUENCY,
 	.suspend_min_freq[2] = DEFAULT_MIN_FREQUENCY,
 	.suspend_min_freq[3] = DEFAULT_MIN_FREQUENCY,
-	.suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME,
 };
