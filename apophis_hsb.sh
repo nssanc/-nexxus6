fc64f278f424f16adecff543d617386d2571ea49
APOPHIS9283 <steaven.android@gmail.com>
2015-11-06 13:22:27 -0500

    Revert "Switch to SM-6.0"
    This reverts commit b4723edebcdc88d3c65ccc512d35ee482eea788c.

--- apophis_hsb.sh
+++ apophis_hsb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ARCH=arm
-export CROSS_COMPILE=/home/apophis9283/gcc/arm-eabi-6.0/bin/arm-eabi-
+export CROSS_COMPILE=/home/apophis9283/gcc/arm-eabi-5.2/bin/arm-eabi-
 make HSB_defconfig
 make -j8
