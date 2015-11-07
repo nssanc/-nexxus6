bfab3a6b288f6620cce48dbcea252cd9efb70ff1
APOPHIS9283 <steaven.android@gmail.com>
2015-11-07 11:22:38 -0500

    Edit LULZACTIVE

--- drivers/cpufreq/cpufreq_lulzactive.c
+++ drivers/cpufreq/cpufreq_lulzactive.c
@@ -83,19 +83,19 @@ static spinlock_t down_cpumask_lock;
 /*
  * The minimum amount of time to spend at a frequency before we can step up.
  */
-#define DEFAULT_UP_SAMPLE_TIME 30000
+#define DEFAULT_UP_SAMPLE_TIME 40000
 static unsigned long up_sample_time;
 
-#define DEFAULT_UP_SAMPLE_TIME_SLEEP 50000
+#define DEFAULT_UP_SAMPLE_TIME_SLEEP 60000
 static unsigned long up_sample_time_awake;
 
 /*
  * The minimum amount of time to spend at a frequency before we can step down.
  */
-#define DEFAULT_DOWN_SAMPLE_TIME 40000
+#define DEFAULT_DOWN_SAMPLE_TIME 20000
 static unsigned long down_sample_time;
 
-#define DEFAULT_DOWN_SAMPLE_TIME_SLEEP 40000
+#define DEFAULT_DOWN_SAMPLE_TIME_SLEEP 20000
 static unsigned long down_sample_time_awake;
 
 /*
@@ -114,7 +114,7 @@ enum {
 /*
  * CPU freq will be increased if measured load > inc_cpu_load;
  */
-#define DEFAULT_INC_CPU_LOAD 75
+#define DEFAULT_INC_CPU_LOAD 80
 static unsigned long inc_cpu_load;
 
 #define DEFAULT_INC_CPU_LOAD_SLEEP 95
@@ -124,7 +124,7 @@ static unsigned long inc_cpu_load_awake;
  * CPU freq will be decreased if measured load < dec_cpu_load;
  * not implemented yet.
  */
-#define DEFAULT_DEC_CPU_LOAD 30
+#define DEFAULT_DEC_CPU_LOAD 40
 static unsigned long dec_cpu_load;
 static unsigned long dec_cpu_load_awake;
 
