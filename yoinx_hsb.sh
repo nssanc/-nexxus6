71b454b9a56adecdbcf8fac7f36a9419ba429930
Joe Schubert <joseph.schubert@gmail.com>
2015-11-06 19:28:12 -0500

    BuildScript: Reset to 5.2 TC. 6.0 was supposed to be in a new branch

--- yoinx_hsb.sh
+++ yoinx_hsb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#export CROSS_COMPILE="ccache /media/joe/linux_storage/toolchains/xanaxdroid-arm-eabi-5.2/bin/arm-eabi-"
-export CROSS_COMPILE="ccache /media/joe/linux_storage/toolchains/sm-arm-eabi-6.0/bin/arm-eabi-"
+export CROSS_COMPILE="ccache /media/joe/linux_storage/toolchains/xanaxdroid-arm-eabi-5.2/bin/arm-eabi-"
+#export CROSS_COMPILE="ccache /media/joe/linux_storage/toolchains/sm-arm-eabi-6.0/bin/arm-eabi-"
 
 export HOST_CC="ccache gcc"
 export CC_JOBS="-j5"
