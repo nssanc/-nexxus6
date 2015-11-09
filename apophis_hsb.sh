8909475fa282a4da2899c325850592c0b4063e78
Joe Schubert <joseph.schubert@gmail.com>
2015-11-09 18:06:49 -0500

    BuildScript: Update Apophis's script to automatically build zip/test_kernel.zip using the repo's zip directory.

--- apophis_hsb.sh
+++ apophis_hsb.sh
@@ -3,3 +3,17 @@ export ARCH=arm
 export CROSS_COMPILE=/home/apophis9283/gcc/arm-eabi-6.0/bin/arm-eabi-
 make HSB_defconfig
 make -j10
+
+
+if [ ! -f arch/arm/boot/zImage-dtb ]; then
+echo "Build Failed"
+else
+echo "Copy kernel and build generate anykernel zip."
+mv arch/arm/boot/zImage-dtb zip/anykernel/kernel/zImage
+
+cd zip/anykernel
+zip -9 -r ../test_kernel.zip .
+cd ../..
+
+echo "Finished Zipping."
+fi
