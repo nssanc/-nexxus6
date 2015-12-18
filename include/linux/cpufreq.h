b8ad03d741dc46a05af8d1659c51916da5e332b3
APOPHIS9283 <steaven.android@gmail.com>
2015-12-17 22:14:09 -0500

    Revert "Add HYPERX governor"
    This reverts commit df45530765d59dbe3363c5149e3ac36cba06d6f8.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -458,9 +458,6 @@ extern struct cpufreq_governor cpufreq_gov_hellsactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_hyper;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_hyper)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPERX)
-extern struct cpufreq_governor cpufreq_gov_hyperx;
-#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_hyperx)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_interactive)
