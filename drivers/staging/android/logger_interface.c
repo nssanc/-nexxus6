6e3ee2f08fbbe8a34bef78c760e57e999df46a48
apophis9283 <steaven.android@gmail.com>
2015-12-31 15:45:48 -0500

    Revert "logger: Add sys fs kernel interface to configure Android logger"
    This reverts commit 14e3fbb8c4322ae492a787a81c7ee9c38be0b249.
    
    Conflicts:
    	drivers/staging/android/Makefile

--- drivers/staging/android/logger_interface.c
+++ drivers/staging/android/logger_interface.c
@@ -29,14 +29,15 @@ static ssize_t logger_mode_show(struct kobject *kobj, struct kobj_attribute *att
 	// print current mode
 	if (logger_mode == 0)
 	{
-		return sprintf(buf, "logger mode: %d (disabled)", logger_mode);
+		return sprintf(buf, "%d", logger_mode);
 	}
 	else
 	{
-		return sprintf(buf, "logger mode: %d (enabled)", logger_mode);
+		return sprintf(buf, "%d", logger_mode);
 	}
 
 }
+
 
 static ssize_t logger_mode_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
@@ -90,8 +91,8 @@ int logger_mode_init(void)
 			kobject_put(logger_mode_kobj);
 	}
 
-	// initialize logger mode to 1 (enabled) as default
-	logger_mode = 1;
+	// initialize logger mode to 0 (disabled) as default
+	logger_mode = 0;
 
         return (logger_mode_retval);
 }
