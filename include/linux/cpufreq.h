1ac5ac9304d267a0eeead6e062ef0560a00fc390
apophis9283 <steaven.android@gmail.com>
2016-01-08 16:54:44 -0500

    All around more fixes and ditch SMARTASSv2

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -509,9 +509,6 @@ extern struct cpufreq_governor cpufreq_gov_preservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_RAGINGMOLASSES)
 extern struct cpufreq_governor cpufreq_gov_ragingmolasses;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ragingmolasses)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
-extern struct cpufreq_governor cpufreq_gov_smartass2;
-#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
 extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartmax_eps)
