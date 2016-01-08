94e5efd2f4dea156e4058c8d70f66fdddd2f9c10
apophis9283 <steaven.android@gmail.com>
2016-01-08 17:43:26 -0500

    Revert "sched: Add a check for cpu unbound deferrable timers"
    This reverts commit 84113ad9a165ab3d19fe9657a591bb2bf175be91.

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -767,11 +767,6 @@ static void __tick_nohz_idle_enter(struct tick_sched *ts)
 
 	now = tick_nohz_start_idle(cpu, ts);
 
-#ifdef CONFIG_SMP
-	if (check_pending_deferrable_timers(cpu))
-		raise_softirq_irqoff(TIMER_SOFTIRQ);
-#endif
-
 	if (can_stop_idle_tick(cpu, ts)) {
 		int was_stopped = ts->tick_stopped;
 
