3eab5f1940b05ffd5150dbcb27c1c7da77a70751
apophis9283 <steaven.android@gmail.com>
2016-01-27 17:51:03 -0500

    Revert "cpufreq: force cpuN policy to match cpu0 when changing freq or gov"
    This reverts commit 94c60125d90eae942bf43e91786970a570732d4e.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2081,7 +2081,6 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 				struct cpufreq_policy *new_policy)
 {
 	int ret = 0, failed = 1;
-	struct cpufreq_policy *cpu0_policy = NULL;
 
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n", new_policy->cpu,
 		new_policy->min, new_policy->max);
@@ -2124,10 +2123,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 			CPUFREQ_NOTIFY, new_policy);
 
 	policy->min = new_policy->min;
-	if (new_policy->cpu) {
-		cpu0_policy = cpufreq_cpu_get(0);
-		policy->max = cpu0_policy->max;
-	} else policy->max = new_policy->max;
+	policy->max = new_policy->max;
 
 	if (likely(allow_minup)) policy->min = new_policy->min;
 	// do not allow mpd or thermal to raise minfreq
@@ -2167,10 +2163,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 			}
 
 			/* start new governor */
-			if (new_policy->cpu && cpu0_policy)
-				policy->governor = cpu0_policy->governor;
-			else
-				policy->governor = new_policy->governor;
+			policy->governor = new_policy->governor;
 			if (!__cpufreq_governor(policy, CPUFREQ_GOV_POLICY_INIT)) {
 				if (!__cpufreq_governor(policy, CPUFREQ_GOV_START)) {
 					failed = 0;
