21adfe62461369eef8a5b2833fcc1ca864471e2e
apophis9283 <steaven.android@gmail.com>
2016-01-29 22:19:04 -0500

    Change TC to SABERMOD-6.0-CORTEX-A15

--- apophis.sh
+++ apophis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export ARCH=arm
-export CROSS_COMPILE=/home/apophis9283/gcc/arm-eabi-6.0/bin/arm-eabi-
+export CROSS_COMPILE=/home/apophis9283/gcc/arm-eabi-6.0/arm-linux-gnueabi-6.0/bin/arm-eabi-
 make Singularity_defconfig
 make -j8
 
