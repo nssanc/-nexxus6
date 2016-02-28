f893849d4cee13881cbe5bcb1d87b9135dbadd9e
apophis9283 <steaven.android@gmail.com>
2016-02-27 23:12:16 -0500

    Change my script back to -j8

--- apophis.sh
+++ apophis.sh
@@ -2,7 +2,7 @@
 export ARCH=arm
 export CROSS_COMPILE=/home/apophis9283/gcc/arm-eabi-6.0/arm-linux-gnueabi-6.0/bin/arm-eabi-
 make Singularity_defconfig
-make -j4
+make -j8
 
 
 if [ ! -f arch/arm/boot/zImage-dtb ]; then
