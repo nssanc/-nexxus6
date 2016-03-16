4716f6a851624ad2885ba20f580827baf20fc93c
apophis9283 <steaven.android@gmail.com>
2016-03-15 23:57:20 -0400

    Revert "Kill Impulse governor"
    This reverts commit 5376a6d988d0738b97351af53cf92b8508b32554.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -470,6 +470,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPERX)
 extern struct cpufreq_governor cpufreq_gov_hyperx;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_hyperx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR(&cpufreq_gov_impulse)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intelliactive)
