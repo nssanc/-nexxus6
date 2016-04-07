55b58040e7fcaf96ebad9036ec3b750f56bc3b04
APOPHIS9283 <steaven.android@gmail.com>
2016-04-07 10:24:57 -0400

    Revert "soc: qcom: limiter: Add toggle to control mpdecision"
    This reverts commit 90052a19156cc2a51af1984559df82433636b75f.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -38,7 +38,6 @@ module_param(allow_maxdown, bool, 0644);
 
 #ifdef CONFIG_MSM_LIMITER
 #include <linux/msm_thermal.h>
-#include <soc/qcom/limiter.h>
 #endif
 
 /**
@@ -484,22 +483,10 @@ show_one(cpu_load, load_at_max);
 static int cpufreq_set_policy(struct cpufreq_policy *policy,
 				struct cpufreq_policy *new_policy);
 
-static bool cpufreq_update_allowed(int mpd)
-{
-#ifdef CONFIG_MSM_LIMITER
-	if (mpd == 0 && limit.mpd_enabled == 0)
-#else
-	if (mpd == 0)
-#endif
-		return false;
-
-	return true;
-}
-
 /**
  * cpufreq_per_cpu_attr_write() / store_##file_name() - sysfs write access
  */
-#define store_one(file_name, object)					\
+#define store_one(file_name, object)			\
 static ssize_t store_##file_name					\
 (struct cpufreq_policy *policy, const char *buf, size_t count)		\
 {									\
@@ -507,7 +494,7 @@ static ssize_t store_##file_name					\
 	struct cpufreq_policy new_policy;				\
 	int mpd = strcmp(current->comm, "mpdecision");			\
 									\
-	if (!cpufreq_update_allowed(mpd))				\
+	if (mpd == 0)							\
 		return ret;						\
 									\
 	ret = cpufreq_get_policy(&new_policy, policy->cpu);		\
@@ -524,7 +511,7 @@ static ssize_t store_##file_name					\
 									\
 	policy->user_policy.object = new_policy.object;			\
 									\
-	ret = cpufreq_set_policy(policy, &new_policy);			\
+	ret = cpufreq_set_policy(policy, &new_policy);		\
 									\
 	return ret ? ret : count;					\
 }
--- include/soc/qcom/limiter.h
+++ include/soc/qcom/limiter.h
@@ -1,27 +1,11 @@
 #define MSM_LIMITER			"msm_limiter"
-#define LIMITER_ENABLED			0
-#define MPD_ENABLED			0
-#if defined(CONFIG_ARCH_MSM8916)
-#define DEFAULT_SUSPEND_FREQUENCY	998400
-#else
-#define DEFAULT_SUSPEND_FREQUENCY	1728000
-#endif
-#if defined(CONFIG_ARCH_APQ8084)
-#define DEFAULT_RESUME_FREQUENCY	2649600
-#elif defined(CONFIG_ARCH_MSM8916)
-#define DEFAULT_RESUME_FREQUENCY	1209600
-#else
-#define DEFAULT_RESUME_FREQUENCY	2265600
-#endif
-#if defined(CONFIG_ARCH_MSM8916)
-#define DEFAULT_MIN_FREQUENCY		200000
-#else
-#define DEFAULT_MIN_FREQUENCY		300000
-#endif
+#define LIMITER_ENABLED			1
+#define DEFAULT_SUSPEND_FREQUENCY	1036800
+#define DEFAULT_RESUME_FREQUENCY	2649600                         
+#define DEFAULT_MIN_FREQUENCY		98300
 
 static struct cpu_limit {
 	unsigned int limiter_enabled;
-	unsigned int mpd_enabled;
 	uint32_t suspend_max_freq;
 	uint32_t resume_max_freq_all;
 	uint32_t resume_max_freq[4];
@@ -31,7 +15,6 @@ static struct cpu_limit {
 	struct notifier_block notif;
 } limit = {
 	.limiter_enabled = LIMITER_ENABLED,
-	.mpd_enabled = MPD_ENABLED,
 	.suspend_max_freq = DEFAULT_SUSPEND_FREQUENCY,
 	.resume_max_freq_all = DEFAULT_RESUME_FREQUENCY,
 	.resume_max_freq[0] = DEFAULT_RESUME_FREQUENCY,
