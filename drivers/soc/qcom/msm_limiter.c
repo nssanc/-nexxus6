fca16deaac1ed9316c9423388543989e34e17def
APOPHIS9283 <steaven.android@gmail.com>
2016-05-27 12:04:56 -0400

    Change MSM_LIMITER settings

--- drivers/soc/qcom/msm_limiter.c
+++ drivers/soc/qcom/msm_limiter.c
@@ -41,7 +41,7 @@
 
 #define DEFAULT_SUSP_MAX_FREQUENCY	960000
 #define DEFAULT_RESUME_MAX_FREQUENCY	2649600
-#define DEFAULT_MIN_FREQUENCY		98300 
+#define DEFAULT_MIN_FREQUENCY		223200
 
 static struct notifier_block notif;
 static unsigned int freq_control = FREQ_CONTROL;
