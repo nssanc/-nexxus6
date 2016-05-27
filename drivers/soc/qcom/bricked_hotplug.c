d6248ef162f9584dbea8668d1fffc86d42d49a42
APOPHIS9283 <steaven.android@gmail.com>
2016-05-27 12:38:17 -0400

    Set BRICKED_HOTPLUG to enabled = 0 as we use STATE_HELPER

--- drivers/soc/qcom/bricked_hotplug.c
+++ drivers/soc/qcom/bricked_hotplug.c
@@ -30,7 +30,7 @@
 #define DEBUG 0
 
 #define MPDEC_TAG			"bricked_hotplug"
-#define HOTPLUG_ENABLED			1
+#define HOTPLUG_ENABLED			0
 #define MSM_MPDEC_STARTDELAY		2000
 #define MSM_MPDEC_DELAY			180
 #define DEFAULT_MIN_CPUS_ONLINE		1
