9e8f517e1ff04118c49473a37a44a706631b9476
APOPHIS9283 <steaven.android@gmail.com>
2016-07-06 04:24:07 -0400

    Update adreno_idler.c

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -38,7 +38,7 @@
    Any workload higher than this will be treated as a non-idle workload.
    Adreno idler will more actively try to ramp down the frequency
    if this is set to a higher value. */
-static unsigned long idleworkload = 6000;
+static unsigned long idleworkload = 5000;
 module_param_named(adreno_idler_idleworkload, idleworkload, ulong, 0664);
 
 /* Number of events to wait before ramping down the frequency.
@@ -58,7 +58,7 @@ module_param_named(adreno_idler_downdifferential, downdifferential, uint, 0664);
 static bool adreno_idler_active = true;
 module_param_named(adreno_idler_active, adreno_idler_active, bool, 0664);
 
-static unsigned int idlecount;
+static unsigned int idlecount = 0;
 
 int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 		 unsigned long *freq)
