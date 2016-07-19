646326a80164e4b1d27cd018d4ec941bfe753c4e
APOPHIS9283 <steaven.android@gmail.com>
2016-07-18 21:21:13 -0400

    Add back ONDEMAND

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -519,6 +519,9 @@ extern struct cpufreq_governor cpufreq_gov_mythx_plug;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
+extern struct cpufreq_governor cpufreq_gov_ondemand;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ondemand)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND_EPS)
 extern struct cpufreq_governor cpufreq_gov_ondemand_eps;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_ondemand_eps)
