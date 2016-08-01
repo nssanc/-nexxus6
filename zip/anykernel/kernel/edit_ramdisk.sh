40d96248a22a719a4f63f62d055ad80b7263b31d
APOPHIS9283 <steaven.android@gmail.com>
2016-08-01 18:47:04 -0400

    Revert "just clean up name function"
    This reverts commit 2cf3d668eb16768b8fb0fd4a8caf8a9595bb210a.

--- zip/anykernel/kernel/edit_ramdisk.sh
+++ zip/anykernel/kernel/edit_ramdisk.sh
@@ -2,7 +2,6 @@
 #ramdisk_gov_sed.sh by show-p1984
 #Features:
 #extracts ramdisk
-#Force SystemLess Root install
 #finds busbox in /system or sets default location if it cannot be found
 #add init.d support if not already supported
 #removes forced encryption
@@ -52,7 +51,7 @@ if grep "SYSTEMLESS=true" $Rless; then
 echo SystemLess Root already forced
 else
 rm /data/.supersu
-echo "SYSTEMLESS=true" >> $Rless
+echo "SYSTEMLESS=true" >> /data/.supersu
 echo SystemLess Root forced
 fi
 
