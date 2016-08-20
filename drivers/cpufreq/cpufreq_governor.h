6638809161d911f7db3a132c02868f3eaade8fae
APOPHIS9283 <steaven.android@gmail.com>
2016-08-20 16:33:02 -0400

    CHILL updates

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -208,6 +208,8 @@ struct cs_dbs_tuners {
 	unsigned int twostep_threshold;
 	unsigned int min_load;
         unsigned int sleep_depth;
+        unsigned int boost_enabled;
+	unsigned int boost_count;
 };
 
 struct ex_dbs_tuners {
